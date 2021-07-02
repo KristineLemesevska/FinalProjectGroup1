c36a766a794c37e1d4c2e71c90524e848abc9efe
Kristine Lemesevska <kristine.lemesevska@gmail.com>
2021-07-02 14:26:07 +0300

    Dianas commit 1

--- src/Kika/Main.java
+++ src/Kika/Main.java
@@ -1,9 +1,136 @@
 package Kika;
 
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        System.out.println(" Hello World");
+        public class Main {
 
-    }
-}
+            public static void main(String[] args) {
+
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.println("Do you want to play or see results? p/r");
+                char play = scanner.next().charAt(0);
+
+                //while loop for allowing to play
+                while (play == 'p') {
+                    System.out.println("Please enter Player's 1 name: ");
+                    String player1 = scanner.nextLine();
+
+                    System.out.println("Please enter Player's 2 name:");
+                    String player2 = scanner.nextLine();
+
+                    // Designing a game board
+
+                    char[][] board = {{'-', '-', '-', '-', '-', '-', '-'},
+                            {'|', ' ', '|', ' ', '|', ' ', '|'},
+                            {'|', '-', '+', '-', '+', '-', '|'},
+                            {'|', ' ', '|', ' ', '|', ' ', '|'},
+                            {'|', '-', '+', '-', '+', '-', '|'},
+                            {'|', ' ', '|', ' ', '|', ' ', '|'},
+                            {'-', '-', '-', '-', '-', '-', '-'}};
+
+                    printBoard(board);
+
+                    // asking for entering X and Y
+
+                    System.out.println("Player 1, please enter 1-9 to place your X: ");
+                    int playerX = scanner.nextInt();
+
+                    placingX(board, playerX);
+                    printBoard(board);
+
+                    System.out.println("Player 2, please enter 1-9 to place your Y: ");
+                    int playerY = scanner.nextInt();
+
+                    placingY(board, playerY);
+                    printBoard(board);
+
+                }
+            }
+
+            // method for printing out the game board
+            public static void printBoard(char [] [] board) {
+                for (char [] row: board) {
+                    for (char c : row) {
+                        System.out.print(c);
+                    }
+                    System.out.println();
+                }
+            }
+
+            // method for placing X
+
+            public static void placingX (char [] [] board, int playerX) {
+                switch (playerX) {
+                    case 1:
+                        board[1][1] = 'X';
+                        break;
+                    case 2:
+                        board[1][3] = 'X';
+                        break;
+                    case 3:
+                        board[1][5] = 'X';
+                        break;
+                    case 4:
+                        board[3][1] = 'X';
+                        break;
+                    case 5:
+                        board[3][3] = 'X';
+                        break;
+                    case 6:
+                        board[3][5] = 'X';
+                        break;
+                    case 7:
+                        board[5][1] = 'X';
+                        break;
+                    case 8:
+                        board[5][3] = 'X';
+                        break;
+                    case 9:
+                        board[5][5] = 'X';
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            // method for placing Y
+
+            public static void placingY (char [] [] board, int playerY) {
+                switch (playerY){
+                    case 1:
+                        board [1] [1] = 'Y';
+                        break;
+                    case 2:
+                        board [1] [3] = 'Y';
+                        break;
+                    case 3:
+                        board [1] [5] = 'Y';
+                        break;
+                    case 4:
+                        board [3] [1] = 'Y';
+                        break;
+                    case 5:
+                        board [3] [3] = 'Y';
+                        break;
+                    case 6:
+                        board [3] [5] = 'Y';
+                        break;
+                    case 7:
+                        board [5] [1] = 'Y';
+                        break;
+                    case 8:
+                        board [5] [3] = 'Y';
+                        break;
+                    case 9:
+                        board [5] [5] = 'Y';
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+
+        }
+
